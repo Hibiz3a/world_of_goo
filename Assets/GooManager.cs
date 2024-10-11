973eb02d379cd48b2c5b711dd4b398efdc499a7b
Hibiz3a <106378987+Hibiz3a@users.noreply.github.com>
2024-10-11 17:30:06 +0200

    Debug lineRenderer

--- Assets/GooManager.cs
+++ Assets/GooManager.cs
@@ -8,8 +8,6 @@ public class GooManager : MonoBehaviour
 {
     private Dictionary<String, GameObject> Goo = new Dictionary<string, GameObject>();
 
-    private GameObject Goo1 = null;
-    private GameObject Goo2 = null;
 
 
     public static GooManager instance;
@@ -34,30 +32,4 @@ public class GooManager : MonoBehaviour
         }
     }
 
-    public void SetGooJoint(GameObject _gameObject, SpringJoint2D _jointUse)
-    {
-        List<GameObject> _goo = new List<GameObject>();
-        _goo.Add(_gameObject);
-
-        if (Goo1 == null)
-        {
-            Goo1 = Goo[_gameObject.name];
-            Debug.Log(Goo1.transform.position);
-        }
-        else if (_gameObject != _goo[0] && Goo2 == null)
-        {
-            Goo2 = Goo[_gameObject.name];
-            Debug.Log(Goo2.transform.position);
-        }
-
-        if (Goo1 != null && Goo2 != null)
-        {
-            _jointUse.connectedBody = Goo2.GetComponent<Rigidbody2D>();
-            _jointUse.connectedAnchor = Vector2.zero;
-            _goo.Clear();
-            Goo1.GetComponent<LineRenderer>().SetPosition(1, Goo2.transform.position);
-            Goo1 = null;
-            Goo2 = null;
-        }
-    }
 }
--- Assets/Script/Goo/Attachment/Attach_Goo.cs
+++ Assets/Script/Goo/Attachment/Attach_Goo.cs
@@ -11,16 +11,16 @@ public class Attach_Goo : MonoBehaviour, IPointerClickHandler
     private CircleCollider2D Cc;
 
     [SerializeField] private int RadiusDetection;
-    [SerializeField] private SpringJoint2D[] springJoints; 
-    [SerializeField] private LineRenderer LineTrace;
-    
-    
+    [SerializeField] private SpringJoint2D[] springJoints;
+    private LineRenderer LineTrace;
+
+
     public static Attach_Goo selectedGoo1;
     private Rigidbody2D rb2d;
     private int JointCount;
     private List<LineRenderer> activeLines = new List<LineRenderer>();
-    
-    
+
+
     private GameObject AttachGoo;
 
     private RaycastHit2D[] hit;
@@ -29,28 +29,17 @@ public class Attach_Goo : MonoBehaviour, IPointerClickHandler
     {
         rb2d = GetComponent<Rigidbody2D>();
         springJoints = GetComponents<SpringJoint2D>();
-        
+        if (this.GetComponent<LineRenderer>())
+        {
+            LineTrace = GetComponent<LineRenderer>();
+            LineTrace.startWidth = 0.1f;
+            LineTrace.endWidth = 0.1f;
+        }
+
         Sj = gameObject.GetComponent<SpringJoint2D>();
         Cc = gameObject.GetComponent<CircleCollider2D>();
     }
 
-    private void Update()
-    {
-        foreach (var line in activeLines)
-        {
-            if (line != null)
-            {
-                SpringJoint2D joint = springJoints[0];  // On suppose qu'il y a un SpringJoint
-                if (joint.connectedBody != null)
-                {
-                    // Mettre à jour la position du LineRenderer avec les positions actuelles des deux objets connectés
-                    line.SetPosition(0, transform.position);  // Position du premier objet
-                    line.SetPosition(1, joint.connectedBody.transform.position);  // Position du corps connecté
-                }
-            }
-        }
-    }
-    
 
     private bool DetectGooInRange()
     {
@@ -79,36 +68,32 @@ public class Attach_Goo : MonoBehaviour, IPointerClickHandler
         {
             if (selectedGoo1 == null)
             {
-
                 selectedGoo1 = this;
                 Debug.Log("Premier objet sélectionné : " + gameObject.name);
             }
             else if (selectedGoo1 != this)
             {
-
                 if (selectedGoo1.JointCount < 2)
                 {
-
                     CreateSpringJoint(selectedGoo1, this);
                 }
                 else if (JointCount < 2)
                 {
-
                     CreateSpringJoint(this, selectedGoo1);
                 }
 
-                selectedGoo1 = null;  
+                selectedGoo1 = null;
             }
         }
     }
-    
+
 
     private void CreateSpringJoint(Attach_Goo goo1, Attach_Goo goo2)
     {
         SpringJoint2D availableJoint = null;
         foreach (var joint in goo1.springJoints)
         {
-            if (joint.connectedBody == null)  
+            if (joint.connectedBody == null)
             {
                 availableJoint = joint;
                 break;
@@ -117,34 +102,54 @@ public class Attach_Goo : MonoBehaviour, IPointerClickHandler
 
         if (availableJoint != null)
         {
+            CreateLineRenderer(goo1, goo2);
             availableJoint.connectedBody = goo2.rb2d;
             availableJoint.autoConfigureConnectedAnchor = false;
             availableJoint.connectedAnchor = Vector2.zero;
-            goo1.JointCount++;  
-            goo2.JointCount++;  
-            
-            CreateLineRenderer(goo1, goo2);
+            goo1.JointCount++;
+            goo2.JointCount++;
+
 
             Debug.Log("Connexion créée entre " + goo1.gameObject.name + " et " + goo2.gameObject.name);
         }
     }
-    
+
     private void CreateLineRenderer(Attach_Goo goo1, Attach_Goo goo2)
     {
-        LineRenderer line = this.AddComponent<LineRenderer>();
-        
-        line.startWidth = 0.1f;
-        line.endWidth = 0.1f;
-        line.positionCount = 2;
-        line.material = new Material(Shader.Find("Sprites/Default"));  // Utilise un matériau par défaut
-        line.startColor = Color.black;
-        line.endColor = Color.black;
+        switch (JointCount)
+        {
+            case 0:
+                goo1.LineTrace.positionCount = 4;
+                goo1.LineTrace.SetPosition(0, goo1.transform.position);
+                goo1.LineTrace.SetPosition(1, goo2.transform.position);
+                break;
+            case 1:
+                goo1.LineTrace.positionCount = goo1.LineTrace.positionCount + 2;
+                goo1.LineTrace.SetPosition(2, goo1.transform.position);
+                goo1.LineTrace.SetPosition(3, goo2.transform.position);
+                break;
+        }
+    }
 
-        // Définir les positions initiales de la ligne
-        line.SetPosition(0, goo1.transform.position);  // Position du premier Goo
-        line.SetPosition(1, goo2.transform.position);  // Position du deuxième Goo
 
-        // Ajouter la ligne à la liste des connexions actives
-        activeLines.Add(line);
+    private void Update()
+    {
+        if (springJoints[0].connectedBody != null)
+        {
+            for (int i = 0; i < springJoints.Length; i++)
+            {
+                SpringJoint2D joint = springJoints[i];
+                if (joint.connectedBody != null)
+                {
+                    LineTrace.SetPosition(0, transform.position);
+                    LineTrace.SetPosition(1, joint.connectedBody.transform.position);
+                    if (joint == springJoints[1])
+                    {
+                        LineTrace.SetPosition(2, transform.position);
+                        LineTrace.SetPosition(3, joint.connectedBody.transform.position);
+                    }
+                }
+            }
+        }
     }
 }
