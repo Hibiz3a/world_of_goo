fa7dd9598ce9fd075c8b1cdee7856fe0705b604b
Hibiz3a <106378987+Hibiz3a@users.noreply.github.com>
2024-10-14 16:12:20 +0200

    Finish Goo Attach, Start and End

--- Assets/GooManager.cs
+++ Assets/GooManager.cs
@@ -1,35 +1,72 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+using System.Threading;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GooManager : MonoBehaviour
 {
-    private Dictionary<String, GameObject> Goo = new Dictionary<string, GameObject>();
+    private int WaterGooCount = 5;
+    private int ElectricGooCount = 5;
+    private int ConstructionGooCount = 5;
+    private int WaterGooCountCurrent;
+    private int ElectricGooCountCurrent;
+    private int ConstructionGooCountCurrent;
+
+    [SerializeField] private GameObject WaterGooGO;
+    [SerializeField] private GameObject ElecticGooGO;
+    [SerializeField] private GameObject ConstructionGooGO;
+
+    [SerializeField] private TextMeshProUGUI ElectricGoo;
+    [SerializeField] private TextMeshProUGUI WaterGoo;
+    [SerializeField] private TextMeshProUGUI ConstructionGoo;
 
 
-
-    public static GooManager instance;
-
-    private void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-        }
-        else
-        {
-            Destroy(this);
-        }
-    }
+    private Vector3 MousePos;
 
     private void Start()
     {
-        for (int i = 0; i < gameObject.transform.childCount; i++)
+        WaterGooCountCurrent = WaterGooCount;
+        ElectricGooCountCurrent = ElectricGooCount;
+        ConstructionGooCountCurrent = ConstructionGooCount;
+        ElectricGoo.text = ElectricGooCountCurrent + "/" + ElectricGooCount;
+        ConstructionGoo.text = ConstructionGooCountCurrent + "/" + ConstructionGooCount;
+        WaterGoo.text = WaterGooCountCurrent + "/" + WaterGooCount;
+    }
+
+    private void Update()
+    {
+    }
+
+    public void WaterGooSpawn(InputAction.CallbackContext _context)
+    {
+        if (WaterGooCountCurrent > 0 && _context.started)
         {
-            Goo.Add(gameObject.transform.GetChild(i).name, gameObject.transform.GetChild(i).gameObject);
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Instantiate(WaterGooGO, mousePos, Quaternion.Euler(0, 0, 0), gameObject.transform);
+            WaterGooCountCurrent--;
+            WaterGoo.text = WaterGooCountCurrent + "/" + WaterGooCount;
         }
     }
 
+    public void ElectricGooSpawn(InputAction.CallbackContext _context)
+    {
+        if (ElectricGooCountCurrent > 0 && _context.started)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Instantiate(ElecticGooGO, mousePos, Quaternion.Euler(0, 0, 0), gameObject.transform);
+            ElectricGooCountCurrent--;
+            ElectricGoo.text = ElectricGooCountCurrent + "/" + ElectricGooCount;
+        }
+    }
+
+    public void ConstructionGooSpawn(InputAction.CallbackContext _context)
+    {
+        if (ConstructionGooCountCurrent > 0 && _context.started)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Instantiate(ConstructionGooGO, mousePos, Quaternion.Euler(0, 0, 0), gameObject.transform);
+            ConstructionGooCountCurrent--;
+            ConstructionGoo.text = ConstructionGooCountCurrent + "/" + ConstructionGooCount;
+        }
+    }
 }
--- Assets/Script/Goo/Attachment/Attach_Goo.cs
+++ Assets/Script/Goo/Attachment/Attach_Goo.cs
@@ -1,70 +1,34 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Attach_Goo : MonoBehaviour, IPointerClickHandler
 {
-    private SpringJoint2D Sj;
-    private CircleCollider2D Cc;
-
     [SerializeField] private int RadiusDetection;
     [SerializeField] private SpringJoint2D[] springJoints;
-    private LineRenderer LineTrace;
+    [SerializeField] private GameObject LinePreafab;
+    [SerializeField] private bool isGooStart;
+    [SerializeField] private bool isGooEnd;
+    [SerializeField] private float connectionRange = 5f;
+    private List<GameObject> LinePreafabList = new List<GameObject>();
+    private List<GameObject> RbConnected = new List<GameObject>();
 
 
     public static Attach_Goo selectedGoo1;
     private Rigidbody2D rb2d;
-    private int JointCount;
-    private List<LineRenderer> activeLines = new List<LineRenderer>();
+    private int JointCount = 0;
 
-
-    private GameObject AttachGoo;
-
-    private RaycastHit2D[] hit;
 
     private void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         springJoints = GetComponents<SpringJoint2D>();
-        if (this.GetComponent<LineRenderer>())
-        {
-            LineTrace = GetComponent<LineRenderer>();
-            LineTrace.startWidth = 0.1f;
-            LineTrace.endWidth = 0.1f;
-        }
-
-        Sj = gameObject.GetComponent<SpringJoint2D>();
-        Cc = gameObject.GetComponent<CircleCollider2D>();
-    }
-
-
-    private bool DetectGooInRange()
-    {
-        RaycastHit2D[] hits = Physics2D.CircleCastAll(gameObject.transform.position, RadiusDetection, Vector2.zero,
-            RadiusDetection);
-        hit = hits;
-        if (hits.Length > 0)
-        {
-            for (int i = 0; i <= hits.Length; i++)
-            {
-                if (hits[i].transform.CompareTag("Goo"))
-                {
-                    Debug.Log("true");
-                    return true;
-                }
-            }
-        }
-
-        return false;
     }
 
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        if (CompareTag("Goo") && DetectGooInRange())
+        if (CompareTag("Goo"))
         {
             if (selectedGoo1 == null)
             {
@@ -90,65 +54,114 @@ public class Attach_Goo : MonoBehaviour, IPointerClickHandler
 
     private void CreateSpringJoint(Attach_Goo goo1, Attach_Goo goo2)
     {
-        SpringJoint2D availableJoint = null;
-        foreach (var joint in goo1.springJoints)
+        float distance = Vector2.Distance(goo1.transform.position, goo2.transform.position);
+
+        if (distance <= connectionRange)
         {
-            if (joint.connectedBody == null)
+            SpringJoint2D availableJoint = null;
+
+
+            if (goo1.isGooStart || goo1.isGooEnd)
             {
-                availableJoint = joint;
-                break;
+                foreach (var joint in goo2.springJoints)
+                {
+                    if (joint.connectedBody == null)
+                    {
+                        availableJoint = joint;
+                        break;
+                    }
+                }
+
+                if (availableJoint != null)
+                {
+                    CreateLineRenderer(goo2, goo1);
+                    availableJoint.connectedBody = goo1.rb2d;
+                    availableJoint.autoConfigureConnectedAnchor = false;
+                    availableJoint.connectedAnchor = Vector2.zero;
+                    goo1.JointCount++;
+                    goo2.JointCount++;
+                }
             }
-        }
 
-        if (availableJoint != null)
-        {
-            CreateLineRenderer(goo1, goo2);
-            availableJoint.connectedBody = goo2.rb2d;
-            availableJoint.autoConfigureConnectedAnchor = false;
-            availableJoint.connectedAnchor = Vector2.zero;
-            goo1.JointCount++;
-            goo2.JointCount++;
+            else if (goo2.isGooStart || goo2.isGooEnd)
+            {
+                foreach (var joint in goo1.springJoints)
+                {
+                    if (joint.connectedBody == null)
+                    {
+                        availableJoint = joint;
+                        break;
+                    }
+                }
 
+                if (availableJoint != null)
+                {
+                    CreateLineRenderer(goo1, goo2);
+                    availableJoint.connectedBody = goo2.rb2d;
+                    availableJoint.autoConfigureConnectedAnchor = false;
+                    availableJoint.connectedAnchor = Vector2.zero;
+                    goo1.JointCount++;
+                    goo2.JointCount++;
+                }
+            }
+            else
+            {
+                foreach (var joint in goo1.springJoints)
+                {
+                    if (joint.connectedBody == null)
+                    {
+                        availableJoint = joint;
+                        break;
+                    }
+                }
+
+                if (availableJoint != null)
+                {
+                    CreateLineRenderer(goo1, goo2);
+                    availableJoint.connectedBody = goo2.rb2d;
+                    availableJoint.autoConfigureConnectedAnchor = false;
+                    availableJoint.connectedAnchor = Vector2.zero;
+                    goo1.JointCount++;
+                    goo2.JointCount++;
+                }
+            }
 
             Debug.Log("Connexion créée entre " + goo1.gameObject.name + " et " + goo2.gameObject.name);
         }
-    }
-
-    private void CreateLineRenderer(Attach_Goo goo1, Attach_Goo goo2)
-    {
-        switch (JointCount)
+        else
         {
-            case 0:
-                goo1.LineTrace.positionCount = 4;
-                goo1.LineTrace.SetPosition(0, goo1.transform.position);
-                goo1.LineTrace.SetPosition(1, goo2.transform.position);
-                break;
-            case 1:
-                goo1.LineTrace.positionCount = goo1.LineTrace.positionCount + 2;
-                goo1.LineTrace.SetPosition(2, goo1.transform.position);
-                goo1.LineTrace.SetPosition(3, goo2.transform.position);
-                break;
+            Debug.Log("Les Goos sont trop éloignés pour se connecter.");
         }
     }
 
 
+    private void CreateLineRenderer(Attach_Goo goo1, Attach_Goo goo2)
+    {
+        GameObject _line = Instantiate(LinePreafab, goo1.transform.position, Quaternion.Euler(0, 0, 0),
+            goo1.transform);
+        LineRenderer _linerender = _line.GetComponent<LineRenderer>();
+
+        _linerender.SetPosition(0, goo1.gameObject.transform.position);
+        _linerender.SetPosition(1, goo2.gameObject.transform.position);
+
+        RbConnected.Add(goo2.gameObject);
+        LinePreafabList.Add(_line);
+    }
+
     private void Update()
     {
-        if (springJoints[0].connectedBody != null)
+        for (int i = 0; i < LinePreafabList.Count; i++)
         {
-            for (int i = 0; i < springJoints.Length; i++)
+            LinePreafabList[i].GetComponent<LineRenderer>()
+                .SetPosition(0, LinePreafabList[i].transform.parent.position);
+            LinePreafabList[i].GetComponent<LineRenderer>().SetPosition(1, RbConnected[i].transform.position);
+        }
+
+        for (int i = 0; i < springJoints.Length; i++)
+        {
+            if (!springJoints[i].isActiveAndEnabled)
             {
-                SpringJoint2D joint = springJoints[i];
-                if (joint.connectedBody != null)
-                {
-                    LineTrace.SetPosition(0, transform.position);
-                    LineTrace.SetPosition(1, joint.connectedBody.transform.position);
-                    if (joint == springJoints[1])
-                    {
-                        LineTrace.SetPosition(2, transform.position);
-                        LineTrace.SetPosition(3, joint.connectedBody.transform.position);
-                    }
-                }
+                Debug.Log("Game Over");
             }
         }
     }
